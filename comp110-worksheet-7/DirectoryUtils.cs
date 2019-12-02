0216b77e0aeb22d3ff92fd89609b9411125d3852
LMR589281 <lukeryan1@virginmedia.com>
2019-12-02 17:48:47 +0000

    fixed logic errors

--- comp110-worksheet-7/DirectoryUtils.cs
+++ comp110-worksheet-7/DirectoryUtils.cs
@@ -7,24 +7,24 @@ using System.Threading.Tasks;
 
 namespace comp110_worksheet_7
 {
-	public static class DirectoryUtils
-	{
-		// Return the size, in bytes, of the given file
-		public static long GetFileSize(string filePath)
-		{
-            long size = FileInfo(filePath).length;    //get the file size of the given file
+    public static class DirectoryUtils
+    {
+        // Return the size, in bytes, of the given file
+        public static long GetFileSize(string filePath)
+        {
+            long size = new System.IO.FileInfo(filePath).Length;    //get the file size of the given file
             return size;    //returns the size
-       		}
+        }
 
-		// Return true if the given path points to a directory, false if it points to a file
-		public static bool IsDirectory(string path)
-		{
-			return File.GetAttributes(path).HasFlag(FileAttributes.Directory);
-		}
+        // Return true if the given path points to a directory, false if it points to a file
+        public static bool IsDirectory(string path)
+        {
+            return File.GetAttributes(path).HasFlag(FileAttributes.Directory);
+        }
 
-		// Return the total size, in bytes, of all the files below the given directory
-		public static long GetTotalSize(string directory)
-		{
+        // Return the total size, in bytes, of all the files below the given directory
+        public static long GetTotalSize(string directory)
+        {
             long size = 0;    //sets the value of size to be 0
             string[] files = Directory.GetFileSystemEntries(directory);    //gets an array of folders and files
             for (int i = 0; files.Length > i; i++)    //loops for the length of the array of folders and files
@@ -35,32 +35,38 @@ namespace comp110_worksheet_7
                 }
                 else     //if the item in the list is not a folder and thus a file
                 {
-                    size +=  GetFileSize(files[i]);    //the size of the file is added to the size value
+                    size += GetFileSize(files[i]);    //the size of the file is added to the size value
                 }
             }
             return size;    //the size value is returned back
         }
 
-		// Return the number of files (not counting directories) below the given directory
-		public static int CountFiles(string directory)
-		{
+        // Return the number of files (not counting directories) below the given directory
+        public static int CountFiles(string directory)
+        {
             int amount_of_files = 0;    //sets the value amount_of_files to be 0
-            string[] folders = Directory.GetDirectories(directory);    //puts all the folders into a new array called "folders" 
-            for (int i=0; folders.Length>i; i++)    //loops for the amount of folders
+            string[] files = Directory.GetFileSystemEntries(directory);    //gets an array of all the files and folders
+            for (int i = 0; files.Length > i; i++)    //loops for the amount of folders
             {
-                amount_of_files += CountFiles(folders[i]);    //checks folders inside folders
+                if (IsDirectory(files[i]) == true)    //checks to see if the current item in the array is a folder
+                {
+                    amount_of_files += CountFiles(files[i]);    //checks folders inside folders 
+                }
+                else    //if the item in the array is not a folder and thus a file then
+                {
+                    amount_of_files += 1;    //increases the amount of files by 1
+                }
             }
             return amount_of_files;    //returns the amount of files
         }
 
-		// Return the nesting depth of the given directory. A directory containing only files (no subdirectories) has a depth of 0.
-		public static int GetDepth(string directory)
-		{
+        // Return the nesting depth of the given directory. A directory containing only files (no subdirectories) has a depth of 0.
+        public static int GetDepth(string directory)
+        {
             int depth = 0;    //sets depth to equal 0
             string[] folders = Directory.GetDirectories(directory);    //makes an array of folders
-            bool loop = true;    //sets loop to be true
             for (int i = 0; folders.Length > i; i++)    //loops for the length of the folders array
-            {  
+            {
                 if (folders.Length >= 1)    //checks if the folder length is bigger than 1
                 {
                     depth += 1;     //increases depth by 1
@@ -70,20 +76,24 @@ namespace comp110_worksheet_7
             return depth;    //returns the depth value
         }
 
-		// Get the path and size (in bytes) of the smallest file below the given directory
-		public static Tuple<string, long> GetSmallestFile(string directory)
-		{
-            long smallest = 9223372036854775807;    //largest long value, so no files size should be larger than this
+        // Get the path and size (in bytes) of the smallest file below the given directory
+        public static Tuple<string, long> GetSmallestFile(string directory, long smallest = long.MaxValue)    //changed the function to take in the smallest value 
+        {
             string location = "";     //sets the location of the files to a empty string
             string[] files = Directory.GetFileSystemEntries(directory);      //makes an array of folders and files
-            for (int i=0 ; files.Length>i; i++)      //loop for the length of the files
+            for (int i = 0; files.Length > i; i++)      //loop for the length of the files
             {
                 if (IsDirectory(files[i]) == true)     //if the item in the array is a folder
                 {
-                    location, smallest = GetSmallestFile(files[i]);     //then we check inside the folder for files NOT WORKING
+                    Tuple<string, long> new_smallest = GetSmallestFile(files[i], smallest);     //then we check inside the folder for files
+                    if (new_smallest.Item2 < smallest)
+                    {
+                        smallest = new_smallest.Item2;
+                        location = new_smallest.Item1;
+                    }
                 }
                 else     //if the item in the array is not a folder and thus a file
-                {     
+                {
                     if (GetFileSize(files[i]) < smallest)     //checks to see if the fil is smaller then the current smallest file
                     {
                         smallest = GetFileSize(files[i]);     //set the new smallest value
@@ -91,21 +101,25 @@ namespace comp110_worksheet_7
                     }
                 }
             }
-            Tuple<string, long> answer = new Tuple<string, long>(location, largest);     //sets up a tuple
+            Tuple<string, long> answer = new Tuple<string, long>(location, smallest);     //sets up a tuple
             return answer;     //returns the tuple
         }
 
-		// Get the path and size (in bytes) of the largest file below the given directory
-		public static Tuple<string, long> GetLargestFile(string directory)
-		{
-            long largest = 0;    //sets longest to be 0
+        // Get the path and size (in bytes) of the largest file below the given directory
+        public static Tuple<string, long> GetLargestFile(string directory, long largest = 0)    //changed the function to take in the largest value 
+        {
             string location = "";     //sets the location of the files to a empty string
             string[] files = Directory.GetFileSystemEntries(directory);      //makes an array of folders and files
             for (int i = 0; files.Length > i; i++)      //loop for the length of the files
             {
                 if (IsDirectory(files[i]) == true)     //if the item in the array is a folder
                 {
-                    location, smallest = GetSmallestFile(files[i]);     //then we check inside the folder for files NOT WORKING
+                    Tuple<string, long> new_largest = GetLargestFile(files[i], largest);     //then we check inside the folder for files 
+                    if (new_largest.Item2 > largest)
+                    {
+                        largest = new_largest.Item2;
+                        location = new_largest.Item1;
+                    }
                 }
                 else     //if the item in the array is not a folder and thus a file
                 {
@@ -119,29 +133,30 @@ namespace comp110_worksheet_7
             Tuple<string, long> answer = new Tuple<string, long>(location, largest);     //sets up a tuple
             return answer;     //returns the tuple
         }
-		}
 
-		// Get all files whose size is equal to the given value (in bytes) below the given directory
-		public static IEnumerable<string> GetFilesOfSize(string directory, long size)
-		{
-        string[] same_size;     //makes an array of strings called "same_size"
-        string[] files = Directory.GetFileSystemEntries(directory);     //gets an array of files
-        for (int i = 0; files.Length > i; i++)     //loops for the length of the files
+
+        // Get all files whose size is equal to the given value (in bytes) below the given directory
+        public static IEnumerable<string> GetFilesOfSize(string directory, long size)
         {
-            if (IsDirectory(files[i]) == true)     //if the item in the array is a folder then
+            List<string> same_size = new List<string> ();     //makes a list of strings called "same_size"
+            string[] files = Directory.GetFileSystemEntries(directory);     //gets an array of files
+            for (int i = 0; files.Length > i; i++)     //loops for the length of the files
             {
-                same_size_plus = GetFileSystemEntries(files[i]);     //we check the folder for files
-                same_size.Add(same_size_plus);     //add any new items to the array
-            }
-            else     //if the item in the array is not a folder and thus a file
-            {
-                if (GetFileSize(files[i]) = size)     //check to see if we have the right size
+                if (IsDirectory(files[i]) == true)     //if the item in the array is a folder then
                 {
-                    same_size.Add(files[i]);     //add the files to the array
+                    IEnumerable<string> same = GetFilesOfSize(files[i], size);     //we check the folder for files
+                    same_size = same_size.Concat(same).ToList();     //add any new items to the list
+                }
+                else     //if the item in the array is not a folder and thus a file
+                {
+                    if (GetFileSize(files[i]) == size)     //check to see if we have the right size
+                    {
+                        same_size.Add(files[i]);     //add the files to the list
+                    }
                 }
             }
+            return same_size;     //return the list
         }
-        return same_size;     //return the list of arrays
     }
 }
 
